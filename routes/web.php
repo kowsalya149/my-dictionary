cc0e72a7ffcace939af51c1c3c36759d022c78b3
Kowsalya <p.r.kowsalya149@gmail.com>
2019-02-23 13:31:25 +0530

    loginForm-Controller

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('form');
 });
+
+Route::resource('form','LoginController');
+
+Route::get('login','LoginController@login');
